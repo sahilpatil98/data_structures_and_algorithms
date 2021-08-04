92e247a60c5bf84e99c465c9127fdae3d1344487
sahilpatil98 <87723800+sahilpatil98@users.noreply.github.com>
2021-08-04 13:16:31 -0700

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 12:55:28 2021
+
+@author: d0tamon
+"""
+
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+    
+    while first <= last:
+        midpoint = (first + last)//2
+        
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+        
+
+        
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = binary_search(numbers, 8)
+verify(result) 
---
+++ linear_search.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 12:33:24 2021
+
+@author: d0tamon
+"""
+
+def linear_search(list, target):
+    """
+    Returns index position of target if found, else None
+    """
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+        
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 4)
+verify(result)
